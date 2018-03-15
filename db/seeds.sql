55c96075f3636d74e02b7e93027512877d97e38b
JS-Quinn <josephshannonquinn@Gmail.com>
2018-03-15 11:37:09 -0400

    database added

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ("Poutine on the Ritz Burger");
+INSERT INTO burgers (burger_name) VALUES ("Poblano Picasso Burger");
+INSERT INTO burgers (burger_name) VALUES ("Beets of Burden Burger");
+INSERT INTO burgers (burger_name) VALUES ("The Human Polenta-pede Burger");
+INSERT INTO burgers (burger_name) VALUES ("Chile Relleno-You-Didn't Burger");
+INSERT INTO burgers (burger_name) VALUES ("Don't You Four Cheddar 'Bout Me Burger");
